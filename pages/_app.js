e1acafb9827673631732731c1ed3b98ba36f19ff
GD Adrian Lansang <gat.lansang@gmail.com>
2022-07-14 18:17:00 +1200

    Set-up Boostrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css' // adds bootstrap css
 import '../styles/globals.scss'
 
+import { useEffect } from "react"
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap"); // adds bootstrap js
+  }, []);
+
   return <Component {...pageProps} />
 }
 
