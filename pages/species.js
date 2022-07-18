f4da23caeec59a1d407520539517acdcaf6c9e7e
GD Adrian Lansang <gat.lansang@gmail.com>
2022-07-18 12:36:57 +1200

    RFA-4 | Implements species list

--- pages/species.js
+++ pages/species.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Head from "next/head";
+import SpeciesList from '../components/species/SpeciesList';
 
-const Species = () => {
+export const getServerSideProps = async () => {
+    const URL = process.env.API_URL + process.env.SPECIES_ENTITY;
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    return {
+        props: {data: data}
+    }
+}
+
+const Species = ({ data }) => {
     return (
         <>
             <Head>
                 <title>Species | Star Wars Dashboard</title>
             </Head>
-            <div className="row">
-                <div className="col-md-12">
-                    <h4>Species</h4>
-                </div>
-            </div>
+            <SpeciesList data={data} />
         </>
     );
 };
